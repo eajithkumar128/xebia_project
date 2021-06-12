e166b5a35cce36f3b3c14a4e929ffe0362a96c6a
ajithkumar.raja@intellectdesign.com <ajithkumar.raja@intellectdesign.com>
2021-06-12 23:54:06 +0530

    changes

--- src/components/BlogContentViewer.vue
+++ src/components/BlogContentViewer.vue
@@ -2,7 +2,9 @@
   <div>
         <v-card
             elevation="2"
+            :class="type!== 'single' ? 'cards' : ''"
             outlined
+            hover
             :style="[type === 'single' ? {'height':'70vh','overflow':'Scroll'} : {}]"
         >
             <v-card-title>{{blog.title}}</v-card-title>
@@ -20,3 +22,11 @@ export default{
     props:['blog','type']
 }
 </script>
+
+<style lang="scss">
+.cards{
+    &:hover{
+        border: 1px solid green;
+    }
+}
+</style>
